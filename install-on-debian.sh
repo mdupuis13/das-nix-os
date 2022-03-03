06b348532f7a4215fc78ce2b1157227a2bd7cf85
Martin Dupuis <4422549+mdupuis13@users.noreply.github.com>
2022-03-02 21:47:13 -0500

    Add debian base install script

---
+++ install-on-debian.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# install git + z shell 
+apt-get install git zsh
+
+# create and move into $HOME/src/$USER
+
+# clone das-nix-os repo
+# clone dotfilez-dasboss-pc repo
+# clone universal-devenv repo
+
+# ask to install X or not
+# if YES run install-x-debian.sh
+
+
+# ask to install devenv
+# if YES run universal-devenv/install.sh
