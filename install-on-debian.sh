d7dd23ba9ab5a3510275c3daf30751f80e4bdb00
Martin Dupuis <4422549+mdupuis13@users.noreply.github.com>
2022-04-09 21:49:21 -0400

    Add script flow questions

--- install-on-debian.sh
+++ install-on-debian.sh
@@ -1,17 +1,62 @@
 #!/usr/bin/env bash
 
 # install git + z shell 
+echo "Installing base programs (git, zsh)"
 apt-get install git zsh zsh-autosuggestions zsh-syntax-highlighting
 
-# create and move into $HOME/src/$USER
+# create folder and move into $HOME/src/$USER
+mkdir -p src/$USER
+cd src/$USER
 
 # clone das-nix-os repo
+git clone https://github.com/mdupuis13/das-nix-os.git
+
 # clone dotfilez-dasboss-pc repo
-# clone universal-devenv repo
+git clone https://github.com/mdupuis13/dotfilez-dasboss-pc.git
+
+
 
 # ask to install X or not
-# if YES run install-x-debian.sh
+echo -n "Install an X environment ? Y/n: "
+read -r p installX
+
+case $installX in
+      [yY][eE][sS]|[yY])
+            echo "Install X Window stuff"
+            # if YES run install-x-on-debian.sh
+            ;;
+      [nN][oO]|[nN])
+            echo "Just install a terminal config"
+            ;;
+      *)
+            echo "Invalid input..."
+            exit 1
+            ;;
+esac
 
 
 # ask to install devenv
-# if YES run universal-devenv/install.sh
+echo -n "Install an X environment ? Y/n: "
+read -r p installX
+
+case $installX in
+      [yY][eE][sS]|[yY])
+            echo "Install dev-env stuff"
+            # if YES clone repo and run universal-devenv/install.sh
+            isntall_devenv
+            ;;
+      [nN][oO]|[nN])
+            echo "Not installing dev-env stuff..."
+            ;;
+      *)
+            echo "Invalid input..."
+            exit 1
+            ;;
+esac
+
+function isntall_devenv() {
+    # clone universal-devenv repo
+    cd src/$USER
+    git clone https://github.com/mdupuis13/universal-devenv.git
+        
+}
